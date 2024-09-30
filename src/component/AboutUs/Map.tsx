fe84437d3a2a287062de9e758d35c946b79da17b
JobaerSiddique <jobaersiddique28me@gmail.com>
2024-09-30 15:43:27 +0600

    checking

--- src/component/AboutUs/Map.tsx
+++ src/component/AboutUs/Map.tsx
@@ -5,7 +5,7 @@ const Map = () => {
         <div className="py-12">
         <h2 className="text-3xl font-bold text-center mb-8">Find Us</h2>
         <div className="flex justify-center">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4332.466405824476!2d90.3811914753354!3d23.733193678682024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b84e57dd8b19%3A0xd66f294182e721e8!2z4Kai4Ka-4KaV4Ka-IOCmqOCmv-CmiSDgpq7gpr7gprDgp43gppXgp4fgpp8!5e1!3m2!1sbn!2sbd!4v1727689143578!5m2!1sbn!2sbd" width="600" height="450"   loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4332.466405824476!2d90.3811914753354!3d23.733193678682024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b84e57dd8b19%3A0xd66f294182e721e8!2z4Kai4Ka-4KaV4Ka-IOCmqOCmv-CmiSDgpq7gpr7gprDgp43gppXgp4fgpp8!5e1!3m2!1sbn!2sbd!4v1727689143578!5m2!1sbn!2sbd" width="600" height="450"   loading="lazy" ></iframe>
         </div>
       </div>
     );
--- src/page/Shared/Navbar.tsx
+++ src/page/Shared/Navbar.tsx
@@ -32,7 +32,9 @@ export const Navbar = () => {
     }, []);
 
     const handleLogout = () => {
+
         dispatch(logOut());
+        window.location.reload(); 
     };
 
     return (
